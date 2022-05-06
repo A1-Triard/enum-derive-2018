9a39dc0960a1786a3f6bb56b6c5471d499c4f005
Warlock <internalmike@gmail.com>
2022-05-06 17:18:18 +0300

    en

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -7,6 +7,7 @@
 
 use core::alloc::Layout;
 use core::panic::PanicInfo;
+use enum_derive_2018::IterVariants;
 use macro_attr_2018::macro_attr;
 #[cfg(not(windows))]
 use libc::exit;
@@ -39,27 +40,18 @@ pub fn rust_oom(_layout: Layout) -> ! {
     unsafe { exit(98) }
 }
 
-trait TypeName {
-    fn type_name() -> &'static str;
-}
-
-macro_rules! TypeName {
-    (() $vis:vis enum $name:ident $($tail:tt)+) => { TypeName! { @impl $name } };
-    (() $vis:vis struct $name:ident $($tail:tt)+) => { TypeName! { @impl $name } };
-    (@impl $name:ident) => {
-        impl TypeName for $name {
-            fn type_name() -> &'static str { stringify!($name) }
-        }
-    };
-}
-
 macro_attr! {
-    #[derive(TypeName!)]
-    struct X;
+    #[derive(IterVariants!(XVariants), Eq, PartialEq, Debug)]
+    enum X { A, B, C, D }
 }
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    assert_eq!(X::type_name(), "X");
+    let mut variants = X::iter_variants();
+    assert_eq!(variants.next(), Some(X::A));
+    assert_eq!(variants.next(), Some(X::B));
+    assert_eq!(variants.next(), Some(X::C));
+    assert_eq!(variants.next(), Some(X::D));
+    assert_eq!(variants.next(), None);
     0
 }
